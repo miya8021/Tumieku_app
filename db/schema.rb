e9cf5b7a7ae5b0d61728cd8245c8871bfb9a22a2
miya <rinn.sasami@gmail.com>
2021-05-26 21:04:57 +0900

    articlesテーブルにexercise:referencesカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_074955) do
+ActiveRecord::Schema.define(version: 2021_05_26_120154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_05_26_074955) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "exercise_id", null: false
+    t.index ["exercise_id"], name: "index_articles_on_exercise_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -49,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_05_26_074955) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "exercises"
   add_foreign_key "articles", "users"
   add_foreign_key "exercises", "users"
 end
