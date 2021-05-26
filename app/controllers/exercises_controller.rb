7b56ba73c8c8e7ba604de80a209937b1c09ab969
miya <rinn.sasami@gmail.com>
2021-05-26 14:47:57 +0900

    エクセルサイズ登録機能を実装

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,5 +1,6 @@
 class ExercisesController < ApplicationController
   def index
+    @exercise = Exercise.new
   end
 
   def show
@@ -9,6 +10,10 @@ class ExercisesController < ApplicationController
   end
 
   def create
+    exercise = Exercise.new(exercise_params)
+    exercise.user_id = current_user.id
+    exercise.save
+    redirect_to exercise_path
   end
 
   def edit
@@ -18,5 +23,11 @@ class ExercisesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:type)
   end
 end
