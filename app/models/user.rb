03167226bc1495ca3efdf0e70c88c910782a2355
miya <rinn.sasami@gmail.com>
2021-05-29 08:35:42 +0900

    特定のユーザーが「いいね！」したメッセージの一覧を取得できるよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :exercises, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :liked_articles, through: :likes, source: :article
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
