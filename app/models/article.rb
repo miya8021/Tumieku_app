e9e3c0ada9fd9bf6e8d61ca187eb3085dea9558d
miya <rinn.sasami@gmail.com>
2021-05-28 19:20:28 +0900

    userとarticleモデルの関連づけ

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :exercise
+  has_many :likes, dependent: :destroy
   validates :day, :body, presence: true
   validates :minutes, presence: true, numericality: { only_integer: true }
 end
