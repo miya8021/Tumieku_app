6c4069d2efb751bbb3d2f3e686873f84f74173cf
miya <rinn.sasami@gmail.com>
2021-05-29 10:58:32 +0900

    ルーティングとコントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'homes#index'
   devise_for :user
-  resources :users, only: [:show]
+  resources :users, only: %i[show]
   resources :articles
   resources :exercises
+  resources :articles do
+    resource :likes, only: %i[create destroy]
+  end
 end
