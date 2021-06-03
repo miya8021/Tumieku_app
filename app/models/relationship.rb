369919022e53e02c97e7e0233508c9090db829bc
miya <rinn.sasami@gmail.com>
2021-06-03 16:05:14 +0900

    モデルのバリデーションと関連付けを設定

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,9 @@
 class Relationship < ApplicationRecord
+  # class_name:"User"を定義し、Userテーブルのレコードを参照
+  belongs_to :following, class_name: 'User'
+  belongs_to :follower, class_name: 'User'
+  validates :follower_id, uniqueness: {
+    scope: :following_id,
+    message: 'は同じユーザーを2回以上フォローできません'
+  }
 end
