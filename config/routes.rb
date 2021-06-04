d6800dd83cd963ee468c51c0582d660a64f7bc2e
miya <rinn.sasami@gmail.com>
2021-06-04 11:29:10 +0900

    ゲストログイン処理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'homes#index'
   devise_for :user
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   resources :exercises
   resources :users, only: %i[show] do
     resource :relationships, only: [:create, :destroy]
