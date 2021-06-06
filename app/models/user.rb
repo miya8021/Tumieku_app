6cb6cf33ff1254f1b334da207859540ae479a7fe
miya <rinn.sasami@gmail.com>
2021-06-06 21:39:02 +0900

    User モデルのモデルスペックの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :name, presence: true, length: { maximum: 30 }
-  validates :objective, length: { maximum: 140 }
+  validates :objective, presence: true, length: { maximum: 140 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 
   mount_uploader :profile_image, ImageUploader
