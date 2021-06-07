cfef82e59e49e511082e283bb3c903daee796dff
miya <rinn.sasami@gmail.com>
2021-06-07 11:05:27 +0900

    Like モデルのモデルスペックの実装

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    subject { like.valid? }
+
+    context 'データが条件を満たすとき' do
+      let(:like) { build(:like) }
+      it '保存できる' do
+        expect(subject).to eq true
+      end
+    end
+    context 'article_id がすでに存在するとき' do
+      let(:user) { create(:user) }
+      let(:article) { create(:article, user_id: user.id) }
+      before do
+        create(:like, user_id: user.id, article_id: article.id)
+      end
+      let(:like) { build(:like, user_id: user.id, article_id: article.id) }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(like.errors.messages[:user_id]).to include 'は同じ投稿に2回以上いいねはできません'
+      end
+    end
+  end
 end
