307ac9a4b3f2701eab42985880251cced9edc8bf
miya <rinn.sasami@gmail.com>
2021-06-07 11:01:30 +0900

    Comment モデルのモデルスペックの実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   describe 'バリデーション' do
+    subject { comment.valid? }
+    context 'データが条件を満たすとき' do
+      let(:comment) { build(:comment) }
+      it '保存できる' do
+        expect(subject).to eq true
+      end
+    end
+    context 'comment が空のとき' do
+      let(:comment) { build(:comment, content: '') }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(comment.errors.messages[:content]).to include 'を入力してください'
+      end
+    end
+    context 'comment が141文字以上のとき' do
+      let(:comment) { build(:comment, content: 'a' * 141) }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(comment.errors.messages[:content]).to include 'は140文字以内で入力してください'
+      end
+    end
+  end
 end
