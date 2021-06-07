d385748d1badc5dd4175dc4a7a996fced7f077d9
miya <rinn.sasami@gmail.com>
2021-06-07 20:13:37 +0900

    Homes に関するリクエストスペックの実装

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET #index' do
+    subject { get(root_path) }
+
+    it 'リクエストが成功する' do
+      subject
+      expect(response).to have_http_status(200)
+    end
   end
 end
